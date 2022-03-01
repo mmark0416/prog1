5f9fdb5a55f79322fe0f0a2e6c27a895631e68db
hallgato <bikaas87@gmail.com>
2022-03-01 17:24:46 +0100

    s

--- drill_14/drill_14.cpp
+++ drill_14/drill_14.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 #include <vector>
+//1. feladat-------
+class B1{
+public:
+    virtual void vf(){std::cout << "B1::vf()\n";}
+    void f(){std::cout << "B1::vf()\n";}
+    //5. feladat-------
+    virtual void pvf() {}
+};
+//2. feladat-----------
+class D1 : public B1{
+public:
+    void vf() override {std::cout << "D1::vf()\n";}
+    void f() {} //Ha ezt ide írjuk felülírja a B1-től örökölt f függvényt
+};
+//4. feladat--------
 
 int main(){
-    virtual vector
+    
+    //1.feladat--------
+    std::cout << "----1 feladat:----\n";
+    B1 b;
+    b.vf();
+    b.f();
+    //2. feladat--------
+    std::cout << "----2 feladat:----\n";
+    D1 d;
+    d.vf();
+    d.f();
+    //3. feladat--------
+    std::cout << "----3 feladat:----\n";
+    B1& b1_ref {d};
+    b1_ref.vf();
+    b1_ref.f();
+    return 0;
 }
