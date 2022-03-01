3da783180651d2926ec8090ec7dba2bc1288f697
hallgato <bikaas87@gmail.com>
2022-03-01 17:42:26 +0100

    s

--- drill_14/drill_14.cpp
+++ drill_14/drill_14.cpp
@@ -14,8 +14,25 @@ public:
     void vf() override {std::cout << "D1::vf()\n";}
     void f() {} //Ha ezt ide írjuk felülírja a B1-től örökölt f függvényt
 };
-//4. feladat--------
-
+//6. feladat--------
+class D2 : public D1{
+public:
+    void pvf() override {}
+};
+//7. feladat--------
+class B2{
+public:
+    virtual void pvf() {}
+};
+class D21 : public B2{
+public:
+    string s = "valami";
+    void pvf() override {std::cout << s << "\n";}
+};
+class D22 : public B2{
+    int sz = 11;
+    void pvf() override
+};
 int main(){
     
     //1.feladat--------
@@ -33,5 +50,11 @@ int main(){
     B1& b1_ref {d};
     b1_ref.vf();
     b1_ref.f();
+    //6. feladat--------
+    std::cout << "----4 feladat:----\n";
+    D2 d2;
+    d2.f();
+    d2.vf();
+    d2.pvf();
     return 0;
 }
