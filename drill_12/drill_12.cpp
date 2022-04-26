5947ec8465fb36dbe66fa96bdd5e5c00232fca79
hallgato <bikaas87@gmail.com>
2022-04-26 16:12:52 +0200

    drilL

--- drill_12/drill_12.cpp
+++ drill_12/drill_12.cpp
@@ -10,7 +10,6 @@ double square(double x){return x*x;}
 
 int main(){
 	using namespace Graph_lib;
-	// 1. Get an empty Simple_window with the size 600 by 400
 	Point tl {100,100};
 
 	int xmax = 600;
